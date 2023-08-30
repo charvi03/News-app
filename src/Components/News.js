35fab5a8c44ea9aebf50395a56b6983e990a4fc9
charvi03 <charvikhurana01@gmail.com>
2023-08-30 12:09:14 +0530

    init

---
+++ src/Components/News.js
@@ -0,0 +1,10 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+
+export class News extends Component {
+  render() {
+    return <div> This is a News component</div>;
+  }
+}
+
+export default News;
