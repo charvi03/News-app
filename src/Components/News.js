00466ced1c2de267d8caed1808954595b063b3e3
charvi03 <charvikhurana01@gmail.com>
2023-09-07 17:54:44 +0530

    init

--- src/Components/News.js
+++ src/Components/News.js
@@ -12,17 +12,51 @@ export class News extends Component {
     };
   }
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=in&apiKey=a3c9cf497c854f399a065ccb7b9fac88&page=1";
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a3c9cf497c854f399a065ccb7b9fac88&page=1&pageSize=${this.props.pageSize}`;
     let data = await fetch(url);
     let parsedData = await data.json();
-    // console.log(parsedData);
-    this.setState({ articles: parsedData.articles });
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
   }
+  handlePrevClick = async () => {
+    console.log("previous");
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a3c9cf497c854f399a065ccb7b9fac88&page=${
+      this.state.page - 1
+    }&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.page - 1,
+      articles: parsedData.articles,
+    });
+  };
+  handleNextClick = async () => {
+    console.log("next");
+    if (
+      this.state.page + 1 >
+      Math.ceil(this.state.totalResults / this.props.pageSize)
+    ) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a3c9cf497c854f399a065ccb7b9fac88&page=${
+        this.state.page + 1
+      }&pageSize=${this.props.pageSize}`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
+    }
+  };
   render() {
     return (
       <div className="container my-3">
-        <h1>New Monk - Top headlines</h1>
+        <h1 className="text-center">New Monk - Top headlines</h1>
         <div className="row">
           {this.state.articles.map((element) => {
             /* console.log(element); */
@@ -42,11 +76,24 @@ export class News extends Component {
           })}
         </div>
         <div className="container d-flex justify-content-between">
-          <button type="button" class="btn btn-dark">
-            Previous
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handlePrevClick}
+          >
+            &larr; Previous
           </button>
-          <button type="button" class="btn btn-dark">
-            Next
+          <button
+            disabled={
+              this.state.page + 1 >
+              Math.ceil(this.state.totalResults / this.props.pageSize)
+            }
+            type="button"
+            className="btn btn-dark"
+            onClick={this.handleNextClick}
+          >
+            Next &rarr;
           </button>
         </div>
       </div>
